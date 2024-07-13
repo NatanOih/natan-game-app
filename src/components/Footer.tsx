3838ab74a215cff1422c27730609c88b63feb983
Natan Oihman <natanoih@gmail.com>
2024-07-13 23:32:00 +0300

    work in preogress

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,8 @@ import CopyMail from "./CopyMail";
 
 export default function Footer() {
   return (
-    <footer className="flex w-full items-center justify-center p-6 text-xl text-slate-200">
+    <footer className="flex w-full flex-row items-center justify-center gap-4 p-6 text-xl text-slate-200">
+      <p>~ still a work in progress ~</p>
       <CopyMail> natanoih@gmail.com </CopyMail>
     </footer>
   );
